cd7dd538b7256b2235a07ba3d3efd84c1bcc1e48
Yurie <cutecupcakeandcute@gmail.com>
2019-01-11 21:58:52 +0900

    issue:cannot  select javascript object by indecies

--- js-scripts/scripts.js
+++ js-scripts/scripts.js
@@ -11,7 +11,7 @@
 // }
 //
 // request.send();
-
+let numberOfPosts = 0;
 
 
 const Url = 'https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000';
@@ -21,30 +21,44 @@ fetch(Url)
    return res.json()
 })
 .then(data => {
-  // let encodedString;
+  numberOfPosts = data.length;
 
   data = data.map(function(post){
     let title = document.createElement('textarea');
     let body = document.createElement('textarea');
     title.innerHTML += post.title;
     body.innerHTML += post.body;
+     console.log({title: title.value, body: body.value, keywords: post.keywords}.length);
     return {title: title.value, body: body.value, keywords: post.keywords};
   })
 
   $('#getPosts').click(function(){
-    Post(data);
+      Post(data);
+      getUserInputValue();
   })
 
   $('#getPosts').keydown(function(event){
     if (event.keyCode === 13){
       Post(data);
+      getUserInputValue();
     }
   })
+  //
+  // $(document).on('ready', function(){
+  //   numberOfPosts = $("div[class*='post']").length;
+  // })
+
+  Search(data);
 
 
 })
 .catch(error => console.log(error))
 
+
+function getUserInputValue () {
+  let userInput = $('#userInput').val()
+  return userInput;
+}
 
 
 function Post (data) {
@@ -52,7 +66,7 @@ function Post (data) {
   let output = '';
   data.forEach(function(element){
     output += `
-      <div class="container" data-keywords="${element.keywords}">
+      <div class="container post" data-keywords="${element.keywords}">
         <div class="row">
         <div class="col">${element.title}</div>
         <div class="col">${element.body}</div>
@@ -61,5 +75,12 @@ function Post (data) {
     `;
   });
   $('#output').html(output);
+  return data;
+}
 
+function Search (data) {
+
+    for (let i = 0; i < numberOfPosts + 1; i++){
+        console.log(data + 'index: ' + i)
+    }
 }
