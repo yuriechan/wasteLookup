c91c1bc600fbd2aa8603f5967da63583d9570862
Yurie <cutecupcakeandcute@gmail.com>
2019-01-12 18:31:32 +0900

    retrieve keyword from each html element and store keyword into array

--- js-scripts/scripts.js
+++ js-scripts/scripts.js
@@ -1,6 +1,7 @@
 
 let numberOfPosts = 0;
-// let dataSet;
+let dataSet = '';
+
 
 const Url = 'https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000';
 
@@ -20,23 +21,24 @@ fetch(Url)
 
     return {title: title.value, body: body.value, keywords: post.keywords};
   })
+  dataSet = data;
+  Post(data);
 
-  // dataSet = data;
 
   $('#getPosts').click(function(){
-      Post(data);
+      // Post(data);
       getUserInputValue();
   })
 
   $('#getPosts').keydown(function(event){
     if (event.keyCode === 13){
-      Post(data);
+      // Post(data);
       getUserInputValue();
     }
   })
 
-
-   Search(data);
+  pushKeywordToArray();
+   // Search();
    // console.log(data[0]['keywords'])
    // console.log(data[0])
 })
@@ -50,34 +52,71 @@ function getUserInputValue () {
 
 
 function Post (data) {
-
+  let index = 0;
   let output = '';
   data.forEach(function(element){
+
     output += `
-      <div class="container post" data-keywords="${element.keywords}">
+      <div style="display:none" class="container post" data-keywords="${element.keywords}" id="${index}">
         <div class="row">
         <div class="col">${element.title}</div>
         <div class="col">${element.body}</div>
         </div>
       </div>
     `;
+
+    index++;
   });
+
   $('#output').html(output);
   return data;
 }
 
-function Search (dataSets) {
+function Search (userInput) {
+  let arr = [];
+  for (let index in dataSet) {
+      let $container = $("div[id*="+ index +"]").data("keywords");
+      console.log($container)
+      arr = $container.split(" ");
+      console.log(arr)
+      // return arr;
+  }
 
-    for (let index in dataSets){
-      console.log('index: ' + index)
-        for (let dataSet of dataSets){
-          console.log(dataSet)
-        }
-    }
+
+
 }
 
-function getKeyWordArray (ArrayOfObject) {
-    for (let index in ArrayOfObject){
-      ArrayOfObject[index]['keywords']
-    }
+function pushKeywordToArray () {
+  let parentArr = [];
+  let childArr = [];
+  for (let index in dataSet) {
+      let $container = $("div[id*="+ index +"]").data("keywords");
+      //console.log($container)
+      childArr = $container.split(", ");
+      parentArr.push(childArr)
+      //console.log(childArr)
+      console.log(parentArr[0])
+      return parentArr;
+  }
+  // console.log(arr[1])
 }
+
+// function searchMatchingPost (userInput, arr) {
+//   for (let )
+// }
+
+
+function storeMatchedPost (twoDimentionalArr, userInput) {
+  let matchedElement = [];
+  for (let index in dataSets){
+    if (userInput == twoDimentionalArr[index]){
+
+    }
+  }
+
+}
+
+// questions:
+// why array is automatically empty after first itiration
+// issue:
+//get rid of comma inside array
