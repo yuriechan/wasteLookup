f9fc4eb611a17f3063f38ac6eb2d7b56d8972f44
Yurie <cutecupcakeandcute@gmail.com>
2019-01-18 18:57:58 +0900

    minor change on css and add folders

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./'));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.listen(process.env.PORT || 4000, function(){
