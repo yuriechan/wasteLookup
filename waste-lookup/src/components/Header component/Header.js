8e311254b9735b49e404e48cf5f509d8139b6b84
Yurie <cutecupcakeandcute@gmail.com>
2019-08-20 16:12:59 +0900

    #9: style header component and import css file into appropriate js file

--- waste-lookup/src/components/Header component/Header.js
+++ waste-lookup/src/components/Header component/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Header.css';
 
 function Header (props) {
 return(
-    <header><h2>{props.title}</h2></header>
+    <header className="Header__container">
+        <h2 className="Header__text">{props.title}</h2>
+    </header>
 )}
   
 export default Header;
