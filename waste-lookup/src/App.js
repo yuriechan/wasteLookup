d5aae844639125951173bb585b7bad3f42903572
Yurie <cutecupcakeandcute@gmail.com>
2019-08-20 14:44:26 +0900

    #12: fetch data from API after view layer is mounted

--- waste-lookup/src/App.js
+++ waste-lookup/src/App.js
@@ -1,7 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    const Url = 'https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000';
+    fetch(Url).then(res => res.json()).then(data => console.log(data));
+  }
+
   render () {
     return (
       <div className="App">
