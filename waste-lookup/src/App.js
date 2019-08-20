c99aad836f2f303680ef838d33d815a5fb04cbf8
Yurie <cutecupcakeandcute@gmail.com>
2019-08-20 14:23:27 +0900

    #12: change functional component into class component

--- waste-lookup/src/App.js
+++ waste-lookup/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
