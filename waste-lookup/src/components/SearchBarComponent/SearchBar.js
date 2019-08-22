3753b24a81dec98bc1cb938cf8ed4cfd959e80fe
Yurie <cutecupcakeandcute@gmail.com>
2019-08-22 14:54:58 +0900

    #10: add search icon from awesome font

--- waste-lookup/src/components/SearchBarComponent/SearchBar.js
+++ waste-lookup/src/components/SearchBarComponent/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = (props) => {
     return (
         <InputGroup>
-        <InputGroupAddon addonType="prepend"></InputGroupAddon>
-        <Input placeholder="search" />
+            <InputGroupAddon>
+                <FontAwesomeIcon icon={faSearch}/>
+            </InputGroupAddon>
+            <Input placeholder="search" />
         </InputGroup>
-    )
-}
+    );
+};
 
 export default SearchBar;
