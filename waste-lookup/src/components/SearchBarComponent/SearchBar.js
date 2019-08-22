d954e6584c431c1f988a093875a83aae68fb13c9
Yurie <cutecupcakeandcute@gmail.com>
2019-08-22 14:57:04 +0900

    #10: add css file and style with flexbox

--- waste-lookup/src/components/SearchBarComponent/SearchBar.js
+++ waste-lookup/src/components/SearchBarComponent/SearchBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SearchBar.css';
 import { InputGroup, InputGroupAddon, Input } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = (props) => {
     return (
-        <InputGroup>
+        <InputGroup className="SearchBar__container">
             <InputGroupAddon>
                 <FontAwesomeIcon icon={faSearch}/>
             </InputGroupAddon>
