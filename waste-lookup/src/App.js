046daf2826e3f6ce90ab73e23a31a723af68b964
Yurie <cutecupcakeandcute@gmail.com>
2019-08-23 12:48:31 +0900

    #10: move all the code inside componentDidMount into a single funtion

--- waste-lookup/src/App.js
+++ waste-lookup/src/App.js
@@ -1,42 +1,55 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Header from './components/HeaderComponent/Header';
-import SearchBar from './components/SearchBarComponent/SearchBar';
-import SearchResults from './components/SearchResultComponent/SearchResult';
+import Header from "./components/HeaderComponent/Header";
+import SearchBar from "./components/SearchBarComponent/SearchBar";
+import SearchResults from "./components/SearchResultComponent/SearchResult";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      json: "",
+    };
+  }
 
   componentDidMount() {
-    const Url = 'https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000';
-    fetch(Url).then(res => res.json()).then(data => console.log(data));
+    const Url = "https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000";
+    fetch(Url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ json: data });
+        console.log(this.state.json);
+      });
   }
 
   search(data, query) {
-    let lowerCaseQuery = query.toLowerCase().replace(/\s/g, '');
-    let matchedArr = new Array();
+    let lowerCaseQuery = query.toLowerCase().replace(/\s/g, "");
+    let matchedArr = [];
 
     for (let i = 0, n = data.length; i < n; i++) {
-      let keywordArr = data[i].keywords.replace(/\s/g, '').split(',');
+      let keywordArr = data[i].keywords.replace(/\s/g, "").split(",");
       let matchedOnce = false;
       for (let j = 0, m = keywordArr.length; j < m; j++) {
         if (lowerCaseQuery === keywordArr[j]) {
           matchedArr.push(i);
           matchedOnce = true;
         }
-        if (matchedOnce) {break;}
+        if (matchedOnce) {
+          break;
+        }
       }
     }
     return matchedArr;
   }
 
-  render () {
+  render() {
     return (
       <div className="App">
-        <Header title="Toronto Waste Lookup"/>
+        <Header title="Toronto Waste Lookup" />
         <div className="SearchSection__container">
-        <SearchBar />
-        <SearchResults />
+          <SearchBar />
+          <SearchResults />
         </div>
       </div>
     );
--- waste-lookup/src/components/SearchResultComponent/SearchResult.js
+++ waste-lookup/src/components/SearchResultComponent/SearchResult.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import './SearchResult.css';
+import React from "react";
+import "./SearchResult.css";
 
-const SearchResult = (props) => {
-    return (
-        <div className="SearchResults__container"></div>
-    )
-}
+const SearchResult = props => {
+  return <div className="SearchResults__container" />;
+};
 
 export default SearchResult;
