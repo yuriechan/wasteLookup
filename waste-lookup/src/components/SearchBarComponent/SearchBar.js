3c21f50072b820af614091db3d3d95ae33214345
Yurie <cutecupcakeandcute@gmail.com>
2019-08-23 13:10:09 +0900

    #10: add button to search bar

--- waste-lookup/src/components/SearchBarComponent/SearchBar.js
+++ waste-lookup/src/components/SearchBarComponent/SearchBar.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './SearchBar.css';
-import { InputGroup, InputGroupAddon, Input } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./SearchBar.css";
+import { InputGroup, InputGroupAddon, Input, Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const SearchBar = (props) => {
-    return (
-        <div className="SearchBar__container">
-        <InputGroup className="SearchBar__wrapper">
-            <InputGroupAddon addonType="prepend">
-                <FontAwesomeIcon icon={faSearch}/>
-            </InputGroupAddon>
-            <Input placeholder="search" />
-        </InputGroup>
-        </div>
-    );
+const SearchBar = props => {
+  return (
+    <div className="SearchBar__container">
+      <InputGroup className="SearchBar__wrapper">
+        <Input placeholder="search" />
+        <InputGroupAddon addonType="append">
+          <Button>
+            <FontAwesomeIcon icon={faSearch} />
+          </Button>
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default SearchBar;
