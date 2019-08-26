92ae5d033935a996e498c5f3444d9831c2b6c1e7
Yurie <cutecupcakeandcute@gmail.com>
2019-08-26 11:37:10 +0900

    #10: add star icon to every item in the data array

--- waste-lookup/src/components/SearchResultComponent/SearchResult.js
+++ waste-lookup/src/components/SearchResultComponent/SearchResult.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./SearchResult.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const SearchResult = props => {
   return (
     <div className="SearchResults__container">
+      <FontAwesomeIcon icon={faStar} />
       <p>{props.title}</p>
       <p>{props.children}</p>
     </div>
