d8a9c4c424606d05985ca7274d7fb0c29795362c
Yurie <cutecupcakeandcute@gmail.com>
2019-08-28 12:48:04 +0900

    #10: rewrite functional component into class component

--- waste-lookup/src/components/SearchBarComponent/SearchBar.js
+++ waste-lookup/src/components/SearchBarComponent/SearchBar.js
@@ -4,19 +4,31 @@ import { InputGroup, InputGroupAddon, Input, Button } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const SearchBar = props => {
-  return (
-    <div className="SearchBar__container">
-      <InputGroup className="SearchBar__wrapper">
-        <Input type="text" placeholder="search" onChange={props.onchange} onKeyDown={props.onkeydown} value={props.value} />
-        <InputGroupAddon addonType="append">
-          <Button onClick={props.onclick}>
-            <FontAwesomeIcon icon={faSearch} />
-          </Button>
-        </InputGroupAddon>
-      </InputGroup>
-    </div>
-  );
-};
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="SearchBar__container">
+        <InputGroup className="SearchBar__wrapper">
+          <Input
+            type="text"
+            placeholder="search"
+            onChange={this.props.onchange}
+            onKeyDown={this.props.onkeydown}
+            value={this.props.value}
+          />
+          <InputGroupAddon addonType="append">
+            <Button onClick={this.props.onclick}>
+              <FontAwesomeIcon icon={faSearch} />
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
--- waste-lookup/src/components/SearchResultComponent/SearchResult.js
+++ waste-lookup/src/components/SearchResultComponent/SearchResult.js
@@ -2,14 +2,19 @@ import React from "react";
 import "./SearchResult.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SearchResult = props => {
-  return (
-    <div onClick={props.onclick} id={props.id} className="SearchResults__container">
-      <FontAwesomeIcon color={props.color} icon="star" />
-      <p>{props.title}</p>
-      <p dangerouslySetInnerHTML={props.children}></p>
-    </div>
-  );
-};
-
+class SearchResult extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div onClick={this.props.onclick} id={this.props.id} className="SearchResults__container">
+        <FontAwesomeIcon color={this.props.color} icon="star" />
+        <p>{this.props.title}</p>
+        <p dangerouslySetInnerHTML={this.props.children}></p>
+      </div>
+    );
+  }
+}
 export default SearchResult;
