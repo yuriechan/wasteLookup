f5d581912adba5dd0f2ba64d304c7520a61ae391
Yurie <cutecupcakeandcute@gmail.com>
2019-10-06 01:35:17 +0900

    #21: change header text to icon when screen size small

--- waste-lookup/src/components/Header/Header.js
+++ waste-lookup/src/components/Header/Header.js
@@ -1,12 +1,38 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Header = () => {
-  return (
-    <header className={styles.Header__container}>
-      <h3 className={styles.Header__text}>Toronto Waste Lookup</h3>
-    </header>
-  );
-};
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+  componentWillMount() {
+    window.addEventListener("resize", this.handleWindowSizeChange);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowSizeChange);
+  }
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+  render() {
+    const { width } = this.state;
+    const isMobile = width <= 500;
+    let headerContent = null;
+    if (isMobile) {
+      headerContent = <FontAwesomeIcon icon={"trash-alt"} />;
+    } else {
+      headerContent = "Toronto Waste Lookup";
+    }
+    return (
+      <header className={styles.Header__container}>
+        <h3 className={styles.Header__text}>{headerContent}</h3>
+      </header>
+    );
+  }
+}
 
 export default Header;
